3597fc23cbc883bc29cc610141d33be9d4967938
Daikon <daikon@daikon.localhost>
2017-09-05 10:43:32 +0700

    Upload to github

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app" class="container is-fullhd">
+    <nav class="navbar has-shadow">
+      <div class="container">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="http://bulma.io">
+            Klinik
+          </a>
+          <div class="navbar-burger burger">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        <div class="navbar-menu">
+          <div class="navbar-start">
+            <a class="navbar-item ">
+              <b-icon icon="address-book"></b-icon>
+              Phòng Khám
+            </a>
+            <a class="navbar-item ">
+              <b-icon icon="address-book"></b-icon>
+              Love
+            </a>
+          </div>
+
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <a class="bd-tw-button button">
+                <span class="icon">
+                  <i class="fa fa-twitter"></i>
+                </span>
+                <span>
+                  User
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <router-view style="margin-top: 10px"></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="tile is-ancestor">
+    <div class="tile is-parent is-vertical is-3 ">
+      <article class="tile is-child hero notification is-success is-fullheight">
+        <div class="content">
+          <p class="subtitle">Danh sách bệnh nhân</p>
+          <div class="content">
+            <div class="control">
+              <b-input>
+              </b-input>
+            </div>
+          </div>
+          <p class="subtitle">Danh sách bệnh nhân</p>
+          <p class="subtitle">Danh sách bệnh nhân</p>
+          <p class="subtitle">Danh sách bệnh nhân</p>
+          <p class="subtitle">Danh sách bệnh nhân</p>
+          <p class="subtitle">Danh sách bệnh nhân</p>
+          <p class="subtitle">Danh sách bệnh nhân</p>
+          
+        </div>
+        
+      </article>
+    </div>
+    <div class="tile is-parent">
+      <article class="tile is-child notification is-primary">
+        <p class="title">Wide tile</p>
+        <p class="subtitle">Aligned with the right tile</p>
+        <div class="content">
+          <!-- Content -->
+        </div>
+      </article>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
